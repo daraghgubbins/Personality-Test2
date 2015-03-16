b47f0d6bc5867fddef78779364801c7db97b436b
daraghgubbins <daragh.gubbins@hotmail.com>
2015-03-16 10:02:11 +0000

    15/03/15

--- extensiveJS.js
+++ extensiveJS.js
@@ -13,6 +13,18 @@ function confirmHome(){
 	}
 }
 
+function toggleOverlay(){
+	var overlay = document.getElementById('overlay');
+	var specialBox = document.getElementById('specialBox');
+	overlay.style.opacity = .8;
+	if(overlay.style.display == "block"){
+		overlay.style.display = "none";
+		specialBox.style.display = "none";
+	} else {
+		overlay.style.display = "block";
+		specialBox.style.display = "block";
+	}
+}
 
 function Store1(int){
 section1=int;
